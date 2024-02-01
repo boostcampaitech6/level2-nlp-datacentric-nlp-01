d1d9747dfbae198e1e3999f1b928cffa01afd782
JAE-HYEONG LEE <71856506+jaealways@users.noreply.github.com>
2024-02-01 10:57:37 +0900

    Update crawler_selenium.py

--- code/crawler_selenium.py
+++ code/crawler_selenium.py
@@ -17,7 +17,7 @@ def get_text(sid1,sid2,date):
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
-    contents = soup.select('#section_body > ul > li > dl > dt > a')
+    contents = soup.select('a > strong')
     array=[]
     for element in contents:
         t=element.text
@@ -36,26 +36,27 @@ news_tag={105:{731,226,227,230,732,283,229,228},101:{259,258,261,771,260,262,310
 # 105, 101, 102, 103, 104, 100
 
 
-sdate = date(2019,1,1)
-edate = date(2021,12,31)
+sdate = date(2017,1,1)
+edate = date(2023,12,31)
 
 list_tag1=list(news_tag.keys())
 list_tag_num=[0,1,2,3,4,6]
-list_date=[(sdate+timedelta(days=x)).strftime('%Y%m%d') for x in range(0,(edate-sdate).days,7)]
+list_date=[(sdate+timedelta(days=x)).strftime('%Y%m%d') for x in range(0,(edate-sdate).days,3)]
 
 
-for n in range(6):
-    list_news = []
-    tag_num,tag1=list_tag_num[n],list_tag1[n]
-    for tag2 in news_tag[tag1]:
-        for date in list_date:
-            try:
-                list_news=list_news+get_text(tag1,tag2,date)
-                list_news=list(set(list_news))
-            except:
-                pass
-    output_news=[x[0] for x in list_news]
-    output_date=[x[1] for x in list_news]
-    art_df=pd.DataFrame({'text':output_news,'date':output_date,'label':[tag_num]*len(output_news)})
-    art_df.to_csv(f'augments_{tag_num}.csv')
+n=0
+list_news = []
+tag_num,tag1=list_tag_num[n],list_tag1[n]
+for tag2 in news_tag[tag1]:
+    for date in list_date:
+        try:
+            list_news=list_news+get_text(tag1,tag2,date)
+            list_news=list(set(list_news))
+        except:
+            pass
+
+output_news=[x[0] for x in list_news]
+output_date=[x[1] for x in list_news]
+art_df=pd.DataFrame({'text':output_news,'date':output_date,'label':[tag_num]*len(output_news)})
+art_df.to_csv(f'augments_{tag_num}.csv')
 
